24e79981fcaa8ed72d06b3671ca23533c22e9a74
Mark Alston <malston@vmware.com>
2024-02-08 19:27:40 +0000

    Stop requesting account id

--- terraform/vault/main.tf
+++ terraform/vault/main.tf
@@ -10,10 +10,11 @@ terraform {
 
     access_key                  = "k-ray"
     secret_key                  = "feedkraystars"
-    region                      = "us-west-1"
+    region                      = "main"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 }
