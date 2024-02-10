b2f3554e94e7237fdb67b1a325da7742e6d6595c
Mark Alston <malston@vmware.com>
2024-02-10 13:04:34 -0700

    Add secrets in one command

--- hack/create-vault-secrets.sh
+++ hack/create-vault-secrets.sh
@@ -6,17 +6,19 @@ set -o pipefail
 
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-# if kubectl get secret vault-token -n external-secrets-operator; then
-#   kubectl delete secret vault-token -n external-secrets-operator
-# fi
-# kubectl create secret generic vault-token --from-literal=token="$VAULT_TOKEN" --namespace=external-secrets-operator
+if kubectl get secret vault-token -n external-secrets-operator; then
+  kubectl delete secret vault-token -n external-secrets-operator
+fi
+kubectl create secret generic vault-token --from-literal=token="$VAULT_TOKEN" --namespace=external-secrets-operator
 
 vault login "$VAULT_TOKEN" -address="$VAULT_ADDR"
 
 echo -n "Enter the route53 accesskeyID: "
 read -r AWS_ACCESS_KEY_ID
-echo "$AWS_ACCESS_KEY_ID" | vault kv put -mount=secret route53 accesskeyid=-
 
 echo -n "Enter the route53 secretAccessKey: "
 read -r AWS_SECRET_ACCESS_KEY
-echo "$AWS_SECRET_ACCESS_KEY" | vault kv put -mount=secret route53 secretaccesskey=-
+
+vault kv put -mount=secret route53 "accesskeyid=$AWS_ACCESS_KEY_ID" "secretaccesskey=$AWS_SECRET_ACCESS_KEY"
+vault kv get -mount=secret -format=json route53
+
