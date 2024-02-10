15684e26d46a9455ca33ad559d3f2cec42ec597c
Mark Alston <malston@vmware.com>
2024-02-10 08:33:01 -0700

    Use full crd name

--- hack/cascade-delete.sh
+++ hack/cascade-delete.sh
@@ -7,5 +7,5 @@ set -o pipefail
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 # argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root' | while read -r i; do argocd app set "$i" --sync-policy none; done
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root' | while read -r i; do kubectl patch app "$i"  -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge; done
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root' | while read -r i; do kubectl delete app "$i"; done
+argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root' | while read -r i; do kubectl patch application "$i"  -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge; done
+argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root' | while read -r i; do kubectl delete application "$i"; done
