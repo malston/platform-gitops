e2bf03f22ac245861561ae0d283483e98f21f1f2
Mark Alston <malston@vmware.com>
2024-02-11 09:12:26 -0700

    Skip requesting account id

--- terraform/users/cibot.tf
+++ terraform/users/cibot.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket   = "state-store"
     key      = "terraform/users/terraform.tfstate"
-    endpoint = "http://minio.minio.svc.cluster.local:9000"
+    endpoint = "https://minio.kubelab.app"
 
     access_key                  = "k-ray"
     secret_key                  = "feedkraystars"
@@ -10,6 +10,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
   required_providers {
