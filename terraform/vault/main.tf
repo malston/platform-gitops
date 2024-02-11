fd2e5ce5491402efc4954451f59bba26cb186a37
Mark Alston <malston@vmware.com>
2024-02-11 16:09:48 +0000

    Update region

--- terraform/vault/main.tf
+++ terraform/vault/main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket   = "state-store"
     key      = "terraform/vault/terraform.tfstate"
-    endpoint = "http://minio.kubelab.app"
+    endpoint = "https://minio.kubelab.app"
 
     access_key                  = "k-ray"
     secret_key                  = "feedkraystars"
