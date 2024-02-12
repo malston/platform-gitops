3106405ab51df7cf44e2aee40b6aff8e8e28f1cd
Mark Alston <malston@vmware.com>
2024-02-12 09:49:00 -0700

    Format code

--- hack/cascade-delete.sh
+++ hack/cascade-delete.sh
@@ -6,7 +6,24 @@ set -o pipefail
 
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root|contour-components' | while read -r i; do argocd app set "$i" --sync-policy none; done
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root|contour-components' | while read -r i; do kubectl patch application "$i"  -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge; done
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root|contour-components' | while read -r i; do kubectl delete application "$i"; done
-argocd app list -o json | jq -r '.[].metadata.name' | grep -vE 'autopilot|argo-cd|root|contour-components' | while read -r i; do argocd app set "$i" --sync-policy auto; done
+argocd app list -o json | \
+  jq -r '.[].metadata.name' | \
+  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  while read -r i; do argocd app set "$i" --sync-policy none; done
+argocd app list -o json | \
+  jq -r '.[].metadata.name' | \
+  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  while read -r i; do kubectl patch application "$i" -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge; done
+argocd app list -o json | \
+  jq -r '.[].metadata.name' | \
+  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  while read -r i; do kubectl delete application "$i"; done
+argocd app list -o json | \
+  jq -r '.[].metadata.name' | \
+  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  while read -r i; do argocd app set "$i" --sync-policy auto; done
+
+
+# kubectl get application "registry" -o json \
+#   | jq 'del(.metadata.finalizers)' \
+#   | kubectl replace -f -
