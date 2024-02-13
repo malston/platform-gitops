b5a719b9b70587215eff850b8c8e3f8d10a8a1ca
Mark Alston <malston@vmware.com>
2024-02-12 21:06:58 -0700

    Check for env vars

--- hack/cascade-delete.sh
+++ hack/cascade-delete.sh
@@ -6,19 +6,19 @@ set -o pipefail
 
 argocd app list -o json | \
   jq -r '.[].metadata.name' | \
-  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  grep -vE 'autopilot|argo-cd|root|contour-mgmt' | \
   while read -r i; do argocd app set "$i" --sync-policy none; done
 argocd app list -o json | \
   jq -r '.[].metadata.name' | \
-  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  grep -vE 'autopilot|argo-cd|root|contour-mgmt' | \
   while read -r i; do kubectl patch application "$i" -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge; done
 argocd app list -o json | \
   jq -r '.[].metadata.name' | \
-  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  grep -vE 'autopilot|argo-cd|root|contour-mgmt' | \
   while read -r i; do kubectl delete application "$i"; done
 argocd app list -o json | \
   jq -r '.[].metadata.name' | \
-  grep -vE 'autopilot|argo-cd|root|contour-components' | \
+  grep -vE 'autopilot|argo-cd|root|contour-mgmt' | \
   while read -r i; do argocd app set "$i" --sync-policy auto; done
 
 # kubectl get application "registry" -o json \
--- hack/create-vault-secrets.sh
+++ hack/create-vault-secrets.sh
@@ -9,15 +9,19 @@ __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 if kubectl get secret vault-token -n external-secrets-operator; then
   kubectl delete secret vault-token -n external-secrets-operator
 fi
-kubectl create secret generic vault-token --from-literal=token="$VAULT_TOKEN" --namespace=external-secrets-operator
+kubectl create secret generic vault-token --from-literal=token="$VAULT_TOKEN" --namespace=cert-manager
 
 echo -n "$VAULT_TOKEN" | vault login -
 
-echo -n "Enter the route53 accesskeyID: "
-read -r AWS_ACCESS_KEY_ID
+if [[ -z $AWS_ACCESS_KEY_ID ]]; then
+  echo -n "Enter the route53 accesskeyID: "
+  read -r AWS_ACCESS_KEY_ID
+fi
 
-echo -n "Enter the route53 secretAccessKey: "
-read -r AWS_SECRET_ACCESS_KEY
+if [[ -z $AWS_SECRET_ACCESS_KEY ]]; then
+  echo -n "Enter the route53 secretAccessKey: "
+  read -rs AWS_SECRET_ACCESS_KEY
+fi
 
 vault kv put -mount=secret route53 "accesskeyid=$AWS_ACCESS_KEY_ID" "secretaccesskey=$AWS_SECRET_ACCESS_KEY"
 vault kv get -mount=secret -format=json route53
